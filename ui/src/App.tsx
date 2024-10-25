6842c193f270b3dc82dfee0d311af8a5a6dd69f0
aeither <36173828+aeither@users.noreply.github.com>
2024-10-25 21:24:36 +0400

    update

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,60 +1,86 @@
-import { ContractProvider } from './providers/Contract';
-import { AgoricProvider } from '@agoric/react-components';
-import { Navbar } from './components/Navbar';
-import { Tabs } from './components/Tabs';
-import { wallets } from 'cosmos-kit';
-import { ThemeProvider, useTheme } from '@interchain-ui/react';
-import '@agoric/react-components/dist/style.css';
-import { useEffect } from 'react';
-// import { Button, Modal } from 'react-daisyui';
+import { AgoricProvider } from "@agoric/react-components";
+import "@agoric/react-components/dist/style.css";
+import { ThemeProvider, useTheme } from "@interchain-ui/react";
+import { wallets } from "cosmos-kit";
+import { useEffect } from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Tabs } from "./components/Tabs";
+import { ContractProvider } from "./providers/Contract";
+
+// Root layout component
+function RootLayout() {
+	const { themeClass, setTheme, setColorMode } = useTheme();
+
+	useEffect(() => {
+		setColorMode("dark");
+		setTheme("dark");
+	}, [setTheme, setColorMode]);
+
+	return (
+		<div className={themeClass}>
+			<Navbar />
+			<Outlet />
+		</div>
+	);
+}
+
+// Router configuration
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Tabs />,
+			},
+			// Add more routes as needed
+			{
+				path: "/some-path",
+				element: <div>Some Component</div>,
+			},
+		],
+	},
+]);
 
 function App() {
-  const { themeClass, setTheme, setColorMode } = useTheme();
-  useEffect(() => {
-    setColorMode('dark');
-    setTheme('dark');
-  }, [setTheme, setColorMode]);
-
-  return (
-    <ThemeProvider>
-      <div className={themeClass}>
-        <AgoricProvider
-          // @ts-expect-error XXX _chainWalletMap' is protected but type 'MainWalletBase' is not a class derived from 'MainWalletBase
-          wallets={wallets.extension}
-          agoricNetworkConfigs={[
-            {
-              testChain: {
-                chainId: 'agoriclocal',
-                chainName: 'agoric-local',
-                iconUrl: 'agoric.svg', // Optional icon for dropdown display
-              },
-              apis: {
-                rest: ['http://localhost:1317'],
-                rpc: ['http://localhost:26657'],
-              },
-            },
-            {
-              testChain: {
-                chainId: 'agoric-emerynet-8',
-                chainName: 'emerynet',
-                iconUrl: 'agoric.svg',
-              },
-              apis: {
-                rest: ['https://emerynet.api.agoric.net'],
-                rpc: ['https://emerynet.rpc.agoric.net'],
-              },
-            },
-          ]}
-          defaultChainName="agoric-local"
-        >
-          <ContractProvider>
-            <Navbar />
-            <Tabs />
-          </ContractProvider>
-        </AgoricProvider>
-      </div>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<AgoricProvider
+				wallets={wallets.extension}
+				agoricNetworkConfigs={[
+					{
+						testChain: {
+							chainId: "agoriclocal",
+							chainName: "agoric-local",
+							iconUrl: "agoric.svg",
+						},
+						apis: {
+							rest: ["http://localhost:1317"],
+							rpc: ["http://localhost:26657"],
+						},
+					},
+					{
+						testChain: {
+							chainId: "agoric-emerynet-8",
+							chainName: "emerynet",
+							iconUrl: "agoric.svg",
+						},
+						apis: {
+							rest: ["https://emerynet.api.agoric.net"],
+							rpc: ["https://emerynet.rpc.agoric.net"],
+						},
+					},
+				]}
+				defaultChainName="agoric-local"
+			>
+				<ContractProvider>
+					<RouterProvider router={router} />
+				</ContractProvider>
+			</AgoricProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
